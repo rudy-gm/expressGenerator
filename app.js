864ee6f24073e96f6189ad38c95c1b8e4cd265f8
rodolfo-gm <gutierrezrodolfo80@gmail.com>
2022-05-15 16:12:13 -0400

    second commit // Express Generator REST API

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionsRouter = require('./routes/promotionsRouter');
+const partnersRouter = require('./routes/partnersRouter');
+
+
 
 var app = express();
 
@@ -21,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionsRouter);
+app.use('/partners', partnersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
